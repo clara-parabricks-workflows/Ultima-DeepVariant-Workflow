233d866a7e3292b89c5f354d2e9ab785f3abd541
Myrieme Demouth <mdemouth@nvidia.com>
2023-12-06 15:22:33 +0100

    Update run.sh
    Updating the command line for Parabrick's DeepVariant and adding quality metrics

--- run.sh
+++ run.sh
@@ -3,25 +3,93 @@
 # TODO: BAM, REF, and ENGINE will need to be stored publicly and downloaded
 # in the final version of this demo 
 
-DATA_DIR="/mnt/1tb_drive/data"
-REF_DIR="/home/gburnett/parabricks/parabricks_sample/Ref"
+DATA_DIR="/data"
+REF_DIR="/data/ultima-GIAB-Oct-2023/parabricks_sample/Ref"
 
-BAM="${DATA_DIR}/HG002_005401-UGAv3-1-CACATCCTGCATGTGAT_rsq_filtered.bam"
+INPUT="$1"
+WGS_COVERAGE="$2"
 REF="${REF_DIR}/Homo_sapiens_assembly38.fasta"
-ENGINE="${DATA_DIR}/trt_engine_b128_opt128.eng"
+
+ENGINE="${DATA_DIR}/a10_ultima_model.eng"
 
 CONTAINER="nvcr.io/nv-parabricks-dev/clara-parabricks:4.1.2-1.ultimaoct"
 
+
+# Run quality metrics 
+docker run --rm \
+        -v ${DATA_DIR}:${DATA_DIR} \
+        -v ${REF_DIR}:${REF_DIR} \
+        us.gcr.io/broad-gotc-prod/picard-cloud:3.0.0 \
+        java -jar /usr/picard/picard.jar \
+        CollectQualityYieldMetrics \
+        INPUT=${INPUT} \
+        R=${REF} \
+        OQ=true \
+        FLOW_MODE=true \
+        OUTPUT=CollectQualityYieldMetrics.txt
+
+docker run --rm \
+        -v ${DATA_DIR}:${DATA_DIR} \
+        -v ${REF_DIR}:${REF_DIR} \
+        us.gcr.io/broad-gotc-prod/picard-cloud:3.0.0 \
+        java -jar /usr/picard/picard.jar \
+        CollectWgsMetrics \
+        INPUT=${INPUT} \
+        VALIDATION_STRINGENCY=SILENT \
+        REFERENCE_SEQUENCE=${REF} \
+        INCLUDE_BQ_HISTOGRAM=true \
+        INTERVALS=${WGS_COVERAGE} \
+        OUTPUT=CollectWgsMetrics.txt \
+        USE_FAST_ALGORITHM=false \
+        COUNT_UNPAIRED=true \
+        COVERAGE_CAP=12500 \
+        READ_LENGTH=250
+        
+docker run --rm \
+        -v ${DATA_DIR}:${DATA_DIR} \
+        -v ${REF_DIR}:${REF_DIR} \
+        us.gcr.io/broad-gotc-prod/picard-cloud:3.0.0 \
+        java -jar /usr/picard/picard.jar \
+        CollectRawWgsMetrics \
+        INPUT=${INPUT} \
+        VALIDATION_STRINGENCY=SILENT \
+        REFERENCE_SEQUENCE=reference \
+        INCLUDE_BQ_HISTOGRAM=true \
+        INTERVALS=${WGS_COVERAGE} \
+        OUTPUT=CollectRawWgsMetrics.txt \
+        USE_FAST_ALGORITHM=false \
+        COUNT_UNPAIRED=true \
+        COVERAGE_CAP=12500 \
+        READ_LENGTH=250
+
+docker run --rm \
+        -v ${DATA_DIR}:${DATA_DIR} \
+        -v ${REF_DIR}:${REF_DIR} \
+         us.gcr.io/broad-gotc-prod/picard-cloud:3.0.0 \
+         java -jar /usr/picard/picard.jar \
+         CollectMultipleMetrics \
+         INPUT=${INPUT} \
+         REFERENCE_SEQUENCE=${REF} \
+         OUTPUT=output_prefix.bam \
+         ASSUME_SORTED=true \
+         PROGRAM="null" \
+         PROGRAM="CollectAlignmentSummaryMetrics" \
+         PROGRAM="CollectGcBiasMetrics" \
+         PROGRAM="QualityScoreDistribution" \
+         METRIC_ACCUMULATION_LEVEL="SAMPLE" \
+         METRIC_ACCUMULATION_LEVEL="LIBRARY"
+         
 # Run DeepVariant using Parabricks 
 
-docker run \
-    --gpus all \ 
-    -v ${DATA_DIR}:${DATA_DIR} \ 
-    -v ${REF_DIR}:${REF_DIR} \ 
+docker run --rm \
+    --gpus all \
+    -v ${DATA_DIR}:${DATA_DIR} \
+    -v ${REF_DIR}:${REF_DIR} \
+    ${CONTAINER} \
     pbrun deepvariant \
-    --ref ${REF} 
-    --in-bam ${BAM} \
-    --out-variants ${DATA_DIR}/out.vcf \
+    --ref ${REF} \
+    --in-bam ${INPUT} \
+    --out-variants ${DATA_DIR}/output.vcf \
     --num-gpus 4 \
     --pb-model-file ${ENGINE} \
     --channel-hmer-deletion-quality \
@@ -47,5 +115,3 @@ docker run \
     --gpu-num-per-partition 1 \
     --num-cpu-threads-per-stream 4 \
     --num-streams-per-gpu 8
-
-# Run quality metrics 
